e0f32d21265eb05f074b9534eea7bab3a371c49c
Hassan H <hkacperh@gmail.com>
2023-11-05 12:10:34 -0600

    changed pw

--- frontierproject/settings.py
+++ frontierproject/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'frontierchallengedb', 
         'USER': 'postgres', 
-        'PASSWORD': 'Marimba1$()',
+        'PASSWORD': 'PASSWORD',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
